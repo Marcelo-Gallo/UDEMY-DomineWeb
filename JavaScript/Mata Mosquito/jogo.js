9135ffe03a755d65f61f1cb187e1e9dfed8eebb1
Marcelo-Gallo <marceloaugustogallo@gmail.com>
2024-05-09 22:25:30 -0300

    JS - App Mata Mosquito add logica de criação de elementos HTML randomica

--- JavaScript/Mata Mosquito/jogo.js
+++ JavaScript/Mata Mosquito/jogo.js
@@ -4,7 +4,27 @@ var largura = 0
 function ajustaTamanhoPalcoJogo() {
     altura = window.innerHeight
     largura = window.innerWidth
-    console.log(altura, largura)
+    console.log(largura, altura)
 }
 
 ajustaTamanhoPalcoJogo()
+
+function posicaoRandomica(){
+    var posicaoX = Math.floor(Math.random() * largura) - 90 //a posicao randomica é, pelo menos, 90px menor que o limite, previnindo que estoure o limite da tela e crie uma barra de rolagem
+    var posicaoY = Math.floor(Math.random() * altura) - 90
+
+    posicaoX = posicaoX < 0 ? 0 : posicaoX //se posicaoX for menor que zero, recebe zero, se não recebe ela mesma
+    posicaoY = posicaoY < 0 ? 0 : posicaoY //mesmo para posicaoY
+
+    console.log(posicaoX, posicaoY)
+
+    //cria o elemento html
+    var mosquito = document.createElement('img')
+    mosquito.src = 'Imagens/mosquito.png'
+    mosquito.className = 'mosquito1'
+    mosquito.style.left = posicaoX + 'px'
+    mosquito.style.top = posicaoY + 'px'
+    mosquito.style.position = 'absolute'
+
+    document.body.appendChild(mosquito)
+}
